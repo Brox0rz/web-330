84765ab4493c1957b3afe911994321560cc171bb
Brox0rz <brox0rz@gmail.com>
2023-11-11 13:26:12 -0800

    added author comments and cleaned up some spacing

--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,4 +1,9 @@
-
+/*  Author: Brock Hemsouvanh
+    Original Theme Author: Professor Krasso
+    Date: 11/11/23
+    food-model.js Description: Module with instructions to call up an array of certain 
+    foods and their calorie counts to be represented on a table grid.
+*/
 
 export class FoodModel {
     constructor(id, name, calories) {
