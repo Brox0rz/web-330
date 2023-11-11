453b7ce66c67d018de2cc73acf3ae04bfee1ed2c
Brox0rz <brox0rz@gmail.com>
2023-11-11 13:02:20 -0800

    adjusted foods so we're not eating our friends or their bodily secretions.

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -2,9 +2,9 @@ import { FoodModel } from './food-model.js';
 
 export class CalorieConverter {
   static data = [
-    new FoodModel(1007, 'Egg', 78),
+    new FoodModel(1007, 'Tofu', 78),
     new FoodModel(1008, 'Apple', 95),
-    new FoodModel(1009, 'Hamburger', 354),
+    new FoodModel(1009, 'Vegan burger', 300),
     new FoodModel(1010, 'Fries', 400),
     new FoodModel(1011, 'Banana', 105),
     new FoodModel(1012, 'Soda', 150)
@@ -47,7 +47,7 @@ document.getElementById('btnSearch').addEventListener('click', function() {
     searchResultsDiv.style.display = 'block'; // Make sure to show the results
   });
   
-  
+
 // Event listener for the Enter key
 document.getElementById('txtFoodItem').addEventListener('keyup', function(event) {
   if (event.key === 'Enter') {
