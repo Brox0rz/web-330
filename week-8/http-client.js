dcae2b7ad75d965a7f554f056372ff36e361b2ff
Brox0rz <brox0rz@gmail.com>
2023-12-10 21:48:18 -0800

    completed week 8 whatabook2 assignment.

--- week-8/http-client.js
+++ week-8/http-client.js
@@ -8,9 +8,27 @@ class HttpClient {
   // setting params to a default value allows the user to get the URL without
   // needing the additional parameter
   async get(url, params = ""){   
-    url = new URL(url)
-    // to do ! #2
-    const res = await fetch(url.toString(), { method: "GET" });
+    url = new URL(url);
+
+    if (params) {
+      const searchParams = new URLSearchParams(params);
+      url.search = searchParams;
+    }
+
+    try {
+      // Step #3 and #4: Fetch the URL and return the response as JSON
+      const res = await fetch(url.toString(), { method: "GET" });
+      // Check if the request was successful
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      // Return the response as JSON
+      return await res.json();
+    } catch (error) {
+      // Handle any errors that occurred during the fetch
+      console.error('Fetch error:', error);
+    }
   }
 }
 
+export { HttpClient };
